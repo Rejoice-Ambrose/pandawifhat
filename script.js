55217fa2c690327328a546aa85329517b89bcdcf
Designs <106755613+Rejoice-Ambrose@users.noreply.github.com>
2024-07-10 23:39:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+        function toggleMenu() {
+            const navMenu = document.getElementById('navMenu');
+            navMenu.style.display = navMenu.style.display === 'flex' ? 'none' : 'flex';
+        }
