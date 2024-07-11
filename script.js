e65421cb2179af2c6f850eca48a6eed9398dc0f0
Designs <106755613+Rejoice-Ambrose@users.noreply.github.com>
2024-07-11 14:09:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,2 @@
 
-        function toggleMenu() {
-            const navMenu = document.getElementById('navMenu');
-            if (navMenu.style.display === 'flex') {
-                navMenu.style.display = 'none';
-            } else {
-                navMenu.style.display = 'flex';
-                navMenu.style.flexDirection = 'column';
-            }
-        }
+       
